7831c274aff25dbc35db00e3cce2df1dfc087922
manutosh <manutoshbeniwal@gmail.com>
2022-10-30 23:47:31 +0530

    t

--- 11half_pyramid_after_180rotation.cpp
+++ 11half_pyramid_after_180rotation.cpp
@@ -4,16 +4,11 @@ int main(){
     int n;
     cout<<"type the value of n"<<endl;
     cin>>n;
-    for (int row=1; row<=n;row++){
-        for(int col=1;col<=n;col++){
-            if (col<=n-row){           //to determine the no of spaces in a row 
-                cout<<' ';
-            }else {
-                cout<<'*';
-            }
+    for (int row=1;row<=n;row++){
+        for (int col=1;col<=n;col++){
+            col<=n-row?cout<<"  ":cout<<"* ";
         }cout<<endl;
-    }
   
-    
+    }
     return 0;
 }
