a60ca1c9dee43d2550f10df3b53388e25f87237d
manutosh <manutoshbeniwal@gmail.com>
2022-11-01 00:04:01 +0530

    update

--- 1apractise.cpp
+++ 1apractise.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
 int main(){
+    int n;
+    cin>>n;
     
+    for (int row=1;row<=n;row++){
+        for (int star=1;star<=row;star++){
+            cout<<"* ";
+        }for (int space=1;space<=(2*n-2*row);space++){
+            cout<<"  ";
+        }
+    }
     
     return 0;
 }
