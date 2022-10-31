b30fac1fd9de625cd7dc30a80cd2b3d7715cc392
manutosh <manutoshbeniwal@gmail.com>
2022-11-01 00:35:56 +0530

    update

--- 13butterfly_pattern.cpp
+++ 13butterfly_pattern.cpp
@@ -4,28 +4,26 @@ int main(){
     int n;
     cout<<"give the value of n"<<endl;
     cin>>n;
-    for (int row=1;row<=n;row++){
-        for(int col=1;col<=row;col++){
-            cout<<'*';
-            }int space=2*n-2*row;
-            for (int j=1;j<=space;j++){
-                cout<<' ';
-            }
-            for(int col=1;col<=row;col++){
-            cout<<'*';}
-        cout<<endl;
-        }
-    for (int row=n;row>=1;row--){
-        for(int col=1;col<=row;col++){
-            cout<<'*';
-            }int space=2*n-2*row;
-            for (int j=1;j<=space;j++){
-                cout<<' ';
-            }
-            for(int col=1;col<=row;col++){
-            cout<<'*';}
-        cout<<endl;
-        }
     
+    for (int row=1;row<=n;row++){
+        for (int star=1;star<=row;star++){
+            cout<<"* ";
+        }for (int space=1;space<=(2*n-2*row);space++){
+            cout<<"  ";
+        }for (int star=1;star<=row;star++){
+            cout<<"* ";
+        }cout<<endl;
+    }
+    for (int row=n;row>=1;row--){
+        for (int star=1;star<=row;star++){
+            cout<<"* ";
+        }
+        for (int space=1;space<=2*n-2*row;space++){
+            cout<<"  ";
+        }
+        for (int star=1;star<=row;star++){
+            cout<<"* ";
+        }cout<<endl;
+    }
 return 0;
 }
--- 1apractise.cpp
+++ 1apractise.cpp
@@ -9,7 +9,20 @@ int main(){
             cout<<"* ";
         }for (int space=1;space<=(2*n-2*row);space++){
             cout<<"  ";
+        }for (int star=1;star<=row;star++){
+            cout<<"* ";
+        }cout<<endl;
+    }
+    for (int row=n;row>=1;row--){
+        for (int star=1;star<=row;star++){
+            cout<<"* ";
         }
+        for (int space=1;space<=2*n-2*row;space++){
+            cout<<"  ";
+        }
+        for (int star=1;star<=row;star++){
+            cout<<"* ";
+        }cout<<endl;
     }
     
     return 0;
