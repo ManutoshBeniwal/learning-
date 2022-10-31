8e37a5af956a4e6315b66ff2fbcc83f9eb951200
manutosh <manutoshbeniwal@gmail.com>
2022-10-31 23:06:46 +0530

    idk

--- 1apractise.cpp
+++ 1apractise.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n;
-    cin>>n;
-    for (int row=1;row<=n;row++){
-        for (int col=1;col<=n;col++){
-            col<=n-row?cout<<"  ":cout<<"* ";
-        }cout<<endl;
-    }
+    
+    
     return 0;
 }
