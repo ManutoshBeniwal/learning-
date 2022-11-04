889ab07550e85803ee91cee91752aa2b48a817a4
manutosh <manutoshbeniwal@gmail.com>
2022-11-05 00:17:43 +0530

    update

--- 1apractise.cpp
+++ 1apractise.cpp
@@ -1,17 +1,7 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main(){
-     int n;
-    cout<<"type the no of rows you want for the series\n";
-    cin>>n;
-    for (int row=1;row<=3;row++){
-        for (int col=1;col<=n;col++){
-            if ((row+col)%4==0 || (row==2 && col%4==0)){
-                cout<<"* ";
-            }else{
-                cout<<"  ";
-            }
-        }cout<<endl;
-    }
+   
     return 0;
 }
---
+++ 25functionquestions.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+void prime(int start, int end){
+    int condition;
+    for (int i=start;i<=end;i++){
+        if (i==0||i==1)
+            continue;
+            condition=1;
+        
+        for (int j=2;j<=i/2;++j){
+            if (i%j==0){
+                condition=0;
+                break;
+                }
+            }
+            if (condition==1){
+                cout<<i<<"  ";
+        }
+    }return;
+}
+int main(){
+    int a,b;
+    cin>>a>>b;
+    prime(a,b);
+    return 0;
+}
