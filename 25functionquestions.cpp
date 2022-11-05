bc33620b9355c56dcaf7c54c566b4085c5d1460a
manutosh <manutoshbeniwal@gmail.com>
2022-11-05 08:01:06 +0530

    update

--- 25functionquestions.cpp
+++ 25functionquestions.cpp
@@ -7,18 +7,11 @@ void prime(int start, int end){
         if (i==0||i==1)
             continue;
             condition=1;
-        
         for (int j=2;j<=i/2;++j){
             if (i%j==0){
                 condition=0;
-                break;
-                }
-            }
-            if (condition==1){
-                cout<<i<<"  ";
-        }
-    }return;
-}
+                break;}}if (condition==1){
+                cout<<i<<"  ";}}return;}
 int main(){
     int a,b;
     cin>>a>>b;
