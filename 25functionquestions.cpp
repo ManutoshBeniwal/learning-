b0ca858fc9d89fa462ae24d2a2c4a82bf1868464
manutosh <manutoshbeniwal@gmail.com>
2022-11-05 08:21:38 +0530

    update

--- 25functionquestions.cpp
+++ 25functionquestions.cpp
@@ -11,10 +11,19 @@ void prime(int start, int end){
             if (i%j==0){
                 condition=0;
                 break;}}if (condition==1){
-                cout<<i<<"  ";}}return;}
+                cout<<i<<"  ";}}cout<<endl;return;}
+void fib(int upto){
+    int a=0;int b=1;int c;
+    for (int count=1;count<=upto;count++){
+        cout<<a<<"  ";c=a+b;
+        a=b;b=c;
+    }return;
+}
 int main(){
     int a,b;
     cin>>a>>b;
     prime(a,b);
+    fib(b);
     return 0;
+    
 }
