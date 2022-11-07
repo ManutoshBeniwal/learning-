dcfd9ea85be827cc27ed53b190c1434289864873
manutosh <manutoshbeniwal@gmail.com>
2022-11-07 18:49:30 +0530

    idk

--- 7remaining_operators.cpp
+++ 7remaining_operators.cpp
@@ -1,24 +1,26 @@
-//bitwise operators
-//& -AND gives true only if both value is true
-//| -OR  gives true when at least one of the conditon is true
-//^ -XOR gives false when both values are same
-//~ -ones complement gives the opposite of the given values
-//<< -(a<<n)=a*2^n
-//>> -(a>>n)=a/2^n
-// assignment operators
-//a+-=b is also a=a+-b same with *+ and /=
-//miscellaneous operators
-//sizeof()- returns the size of variable
-//ternary operator- condition?x:y returns value of X if condition is true or else value of Y ****
+/*bitwise operators
+& -AND gives true only if both value is true
+| -OR  gives true when at least one of the conditon is true
+^ -XOR gives false when both values are same
+~ -ones complement gives the opposite of the given values
+<< -(a<<n)=a*2^n
+>> -(a>>n)=a/2^n
+ assignment operators
+a+-=b is also a=a+-b same with *+ and /=
+miscellaneous operators
+sizeof()- returns the size of variable
+ternary operator- condition?x:y returns value of X if condition is true or else value of Y ****
+
+pythagorian triplet means square the biggest number out of 
+the triplet is equal to the sum of squares of other two
+like the pythagorus theorem
 
 
-
-
-//cast - convert one datatype to another 
-//comma(,)- asigns the last value from a list
-//& - reference operator returns address of the variable in the memory
-//* - pointer operator gives a pointer where a varible is stored in the memory
-//bodmas rule of operators is in operators precedence table lec 9
+cast - convert one datatype to another 
+comma(,)- asigns the last value from a list
+& - reference operator returns address of the variable in the memory
+* - pointer operator gives a pointer where a varible is stored in the memory
+bodmas rule of operators is in operators precedence table lec 9*/
 #include <iostream>
 using namespace std;
 int main(){
--- pythagorian_triplet.cpp
+++ pythagorian_triplet.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-#include <cmath>
 bool check (int x,int y, int z){
     int answer,a,b,c;a=max(x,max(y,z));
     if (a==x){b=y;c=z;}
@@ -11,5 +10,4 @@ bool check (int x,int y, int z){
 int main(){int a,b,c;cin>>a>>b>>c;
     if (check(a,b,c)){cout<<"pythagorian triplet"<<endl;}
     else {cout<<"not a pythagorian triplet"<<endl;}
-    return 0;
-}
+    return 0;}
