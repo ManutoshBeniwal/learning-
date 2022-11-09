3f120e706d48c0bb1f6f7dd7419da733e67f3340
manutosh <manutoshbeniwal@gmail.com>
2022-11-09 18:06:18 +0530

    nothing

--- 1apractise.cpp
+++ 1apractise.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
 using namespace std;
 #include <cmath>
-bool check (int x,int y, int z){
-    int answer,a,b,c;a=max(x,max(y,z));
-    if (a==x){b=y;c=z;}
-    else if (a==y){b=x;c=z;}
-    else {b=x;c=y;}
-    if (a*a==b*b+c*c){return true;}
-    else{return false;}}
-int main(){int a,b,c;cin>>a>>b>>c;
-    if (check(a,b,c)){cout<<"pythagorian triplet"<<endl;}
-    else {cout<<"not a pythagorian triplet"<<endl;}
-    return 0;
+
+int main(){
+    cout<< "this is a \ test "<<endl;
 }
