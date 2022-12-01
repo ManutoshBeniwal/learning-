1fc4992812085f4885323e8ae445f86c21a5bfcc
manutosh <manutoshbeniwal@gmail.com>
2022-12-01 18:26:38 +0530

    update

--- 1apractise.cpp
+++ 1apractise.cpp
@@ -1,37 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 #include <cmath>
-int reverse_no(int n){
-    int lastdigit,ans;
-        while (n>0){lastdigit=n%10;n/=10;
-        ans=ans*10+lastdigit;}return ans;}
-int binarytodecimal(int n){
-    int lastdigit,ans=0,power=1;
-    while (n>0){lastdigit=n%10;ans+=lastdigit*power;
-    power*=2;n/=10;}return ans;}
-int decimaltobinary(int n){
-    int ans=0,quotient=n;
-    while (quotient>0){ans=(ans*10)+(quotient%2);
-    quotient/=2;}
-    return reverse_no(ans);}   
-string decimaltohexadecimal(int n){
-    string ans="";int base=1;
-    while (base<=n){base*=16;}base/=16;
-    while (n>0){int quotient=n/base;n-=quotient*base;
-    base/=16;if (quotient<=9){ans+=to_string(quotient);}
-    else{char c='A'+ quotient-10;ans.push_back(c);}}
-    return ans;}
-int hexadecimaltodecimal(string n){int s=n.size(),base=1,ans;
-    for (int index=s-1;index>=0;index--){
-        if (n[index]<='9'){ans+=(n[index]-'0')*base;}
-        else if (n[index]>='A' && n[index]<='F'){
-            ans+=(n[index]-'A'+10)*base;
-        }base*=16;
-    }return ans;}
-
-    
+int binary_search(int arr[],int n,int key){
+    int s=0,e=n;
+    while (s<=e){int mid=(s+e)/2;
+    if(key==arr[mid]){return mid;}
+    else if (key<arr[mid]){e=mid-1;}
+    else if (key>arr[mid]){s=mid+1;}}return -1;
+}
 int main(){
-    string n;cin>>n;
-    cout<<hexadecimaltodecimal(n)<<endl;
+    int n;cin>>n;int arr[n];for (int i=0;i<n;i++){
+        cin>>arr[i];}int key;cin>>key;
+        cout<<binary_search(arr,n,key)<<endl;
 return 0;
 }
