fe8464a58bafe0cde2c9d8277a727d3c594cb5d2
Manutosh Beniwal <manutoshbeniwal@gmail.com>
2023-08-24 11:22:26 +0530

    t

---
+++ t2raining.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main(){
+   /* int a;char ans;
+    printf ("!!! WELCOME TO THE QUIZ  !!!!\n\n");
+    
+        printf("Q1. where is the lake fateh sagar located?\n a)Jaipur   b)kota\n c)Udaipur  c)Jaiselmer\n");
+        printf("\ntype the option here");
+        scanf("%c",&ans);
+        if (ans=='c'){
+            printf("\nYAY !! you won Rs5000 ");
+        }else {printf("oops!! you lost the game:(");}*/
+
+
+        int percentage;
+        printf("type a percentage to get the grade \n");
+        scanf("%d",&percentage);
+        if (percentage>90){
+            printf("grade= A+");
+        }else if(percentage>80 && percentage<=90){
+            printf("grade= A");
+        }else if(percentage>70 && percentage<=80){
+            printf("grade= B+");
+        }else if (percentage>60 && percentage<=70){
+            printf("grade= B");
+        }else {
+            printf("NAHH!! fail !!! \nn ");
+        }
+}
--- trainning.c
+++ trainning.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 void main(){
-    int a =1,b=1,c=6;
-        int k = !!c;
-        printf("\n%d",k);
+    float unit,ans,bill;
+    printf("give the units comsumed over the month\n");
+    scanf("%f",&unit);
+
+    if (unit<=50){
+        ans=unit;
+    }
+    else if(unit>50 && unit <=100){
+        ans=(unit-50)*1.5+50;
+        
+    }
+    else if (unit >100 && unit <=150){
+        ans = (unit-100)*2+50+50*1.5;
+        
+    }
+    else if (unit >150 && unit <=200 ){
+        ans =(unit-150)*2.5+50+50*1.5+50*2;
+       
+    }
+    else {
+        ans = (unit-200)*3+50+50*1.5+50*2+50*2.5;
+        
+    }
+    
+    bill=0.18*ans+ans;
+    printf("\nyour bill is= %f",bill);
+    
 
 }
